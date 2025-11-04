53f7953fd9a2e98e1871ac1e8e12d4bd1962f2f6
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-04 22:09:29 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/$USER && git clone https://github.com/claudemods/claudemods-burgundy-konsole
-cp -r /home/$USER/claudemods-cyan-konsole/claudemods-burgundy.colorscheme /home/$USER/.local/share/konsole
-cp -r /home/$USER/claudemods-cyan-konsole/claudemods-burgundy.profile /home/$USER/.local/share/konsole
+cp -r /home/$USER/claudemods-burgundy-konsole/claudemods-burgundy.colorscheme /home/$USER/.local/share/konsole
+cp -r /home/$USER/claudemods-burgundy-konsole/claudemods-burgundy.profile /home/$USER/.local/share/konsole
 sed -i '/^\[Desktop Entry\]/,/^\[/ s/^DefaultProfile=.*/DefaultProfile=claudemods-burgundy.profile/' ~/.config/konsolerc
 rm -rf /home/$USER/claudemods-burgundy-konsole
 sleep 1
